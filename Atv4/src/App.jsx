22dc96bbd0098558a88bbdfe964c2556cb25726e
AlessandroON <Alessandrooliveira645.ao@gmail.com>
2025-07-18 10:30:27 -0300

    quase terminado

--- Atv4/src/App.jsx
+++ Atv4/src/App.jsx
@@ -1,11 +1,26 @@
-
 import React from 'react';
 import AstronautForm from './formulario/AstronautForm';
+import './App.css';
 
 function App() {
   return (
-    <div>
-      <AstronautForm />
+    <div className="container">
+      <header>
+        🚀 <strong>AstroKappa</strong> – No mundo da lua
+      </header>
+      
+      <main>
+        <AstronautForm />
+      </main>
+      
+      <footer>
+        <p>🔗 Redes Sociais:</p>
+        <ul>
+          <li><a href="#">Instagram</a></li>
+          <li><a href="#">X (Twitter)</a></li>
+          <li><a href="#">Facebook</a></li>
+        </ul>
+      </footer>
     </div>
   );
 }
